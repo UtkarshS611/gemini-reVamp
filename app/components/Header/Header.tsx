bf895dfa2a9438455593acd59ef960eb8b95b64c
Utkarsh Singh <utkarsh.singh6113@gmail.com>
2025-04-13 11:58:22 +0530

    header added

---
+++ app/components/Header/Header.tsx
@@ -0,0 +1,228 @@
+"use client";
+
+import { MenuIcon } from "lucide-react";
+
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+import { Button } from "@/components/ui/button";
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu";
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import Image from "next/image";
+
+const Header = () => {
+  const features = [
+    {
+      title: "Tracks",
+      description: "Overview of your activity",
+      href: "#",
+    },
+    {
+      title: "Prizes",
+      description: "Awesome prizes awaits you!!!",
+      href: "#",
+    },
+    {
+      title: "Sponsors",
+      description: "Check out our sponsors",
+      href: "#",
+    },
+  ];
+
+  return (
+    <section className="py-4 flex justify-center">
+      <div className="w-full">
+        <nav className="flex items-center justify-between">
+          <a
+            href="https://www.shadcnblocks.com"
+            className="flex items-center gap-2"
+          >
+            <Image
+              height={60}
+              width={250}
+              src="/logo.png"
+              alt="Shadcn UI Navbar"
+            />
+          </a>
+          <NavigationMenu className="hidden lg:block">
+            <NavigationMenuList>
+              <NavigationMenuItem>
+                <NavigationMenuTrigger>More</NavigationMenuTrigger>
+                <NavigationMenuContent>
+                  <div className="grid w-[600px] grid-cols-2 p-3">
+                    {features.map((feature, index) => (
+                      <NavigationMenuLink
+                        href={feature.href}
+                        key={index}
+                        className="rounded-md p-3 transition-colors hover:bg-muted/70"
+                      >
+                        <div key={feature.title}>
+                          <p className="mb-1 font-semibold text-foreground">
+                            {feature.title}
+                          </p>
+                          <p className="text-sm text-muted-foreground">
+                            {feature.description}
+                          </p>
+                        </div>
+                      </NavigationMenuLink>
+                    ))}
+                  </div>
+                </NavigationMenuContent>
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+                <NavigationMenuLink
+                  href="#"
+                  className={navigationMenuTriggerStyle()}
+                >
+                  Home
+                </NavigationMenuLink>
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+                <NavigationMenuLink
+                  href="#"
+                  className={navigationMenuTriggerStyle()}
+                >
+                  About
+                </NavigationMenuLink>
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+                <NavigationMenuLink
+                  href="#"
+                  className={navigationMenuTriggerStyle()}
+                >
+                  Devcation&apos;25
+                </NavigationMenuLink>
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+                <NavigationMenuLink
+                  href="#"
+                  className={navigationMenuTriggerStyle()}
+                >
+                  Timeline
+                </NavigationMenuLink>
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+                <NavigationMenuLink
+                  href="#"
+                  className={navigationMenuTriggerStyle()}
+                >
+                  Events
+                </NavigationMenuLink>
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+                <NavigationMenuLink
+                  href="#"
+                  className={navigationMenuTriggerStyle()}
+                >
+                  Contacts
+                </NavigationMenuLink>
+              </NavigationMenuItem>
+            </NavigationMenuList>
+          </NavigationMenu>
+          <div className="hidden items-center gap-4 lg:flex">
+            <Button>
+              <a href="#">Discord</a>
+            </Button>
+          </div>
+          <Sheet>
+            <SheetTrigger asChild className="lg:hidden">
+              <Button variant="outline" size="icon">
+                <MenuIcon className="h-4 w-4" />
+              </Button>
+            </SheetTrigger>
+            <SheetContent side="top" className="max-h-screen overflow-auto">
+              <SheetHeader>
+                <SheetTitle>
+                  <a
+                    href="https://www.shadcnblocks.com"
+                    className="flex items-center gap-2"
+                  >
+                    <Image
+                      height={62}
+                      width={300}
+                      src="/logo.png"
+                      alt="Shadcn UI Navbar"
+                    />
+                  </a>
+                </SheetTitle>
+              </SheetHeader>
+              <div className="flex flex-col p-4">
+                <Accordion type="single" collapsible className="mt-4 mb-2">
+                  <AccordionItem value="solutions" className="border-none">
+                    <AccordionTrigger className="text-base hover:no-underline">
+                      More
+                    </AccordionTrigger>
+                    <AccordionContent>
+                      <div className="grid md:grid-cols-2">
+                        {features.map((feature, index) => (
+                          <a
+                            href={feature.href}
+                            key={index}
+                            className="rounded-md p-3 transition-colors hover:bg-muted/70"
+                          >
+                            <div key={feature.title}>
+                              <p className="mb-1 font-semibold text-foreground">
+                                {feature.title}
+                              </p>
+                              <p className="text-sm text-muted-foreground">
+                                {feature.description}
+                              </p>
+                            </div>
+                          </a>
+                        ))}
+                      </div>
+                    </AccordionContent>
+                  </AccordionItem>
+                </Accordion>
+                <div className="flex flex-col gap-6">
+                  <a href="#" className="font-medium">
+                    Home
+                  </a>
+                  <a href="#" className="font-medium">
+                    About
+                  </a>
+                  <a href="#" className="font-medium">
+                    Devcathon'25
+                  </a>
+                  <a href="#" className="font-medium">
+                    Timeline
+                  </a>
+                  <a href="#" className="font-medium">
+                    Events
+                  </a>
+                  <a href="#" className="font-medium">
+                    Contacts
+                  </a>
+                </div>
+                <div className="mt-6 flex flex-col gap-4">
+                  <Button>
+                    <a href="#">Discord</a>
+                  </Button>
+                </div>
+              </div>
+            </SheetContent>
+          </Sheet>
+        </nav>
+      </div>
+    </section>
+  );
+};
+
+export default Header;
